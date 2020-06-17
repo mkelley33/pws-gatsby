deaace9a0304af3f8f04f717f604f51019ba5270
Michaux Kelley <michauxkelley@gmail.com>
2020-06-16 22:29:17 -0400

    Update about me page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Layout from '@components/layout';
 
 export default () => (
-  <div>
+  <Layout>
     <h1>About me</h1>
-    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
-  </div>
+    <p>
+      I’m a Software Developer, modern dude, natural-born-world-shaker, aspiring lepidopterist, philately, punk, writer,
+      poet, and JavaScript hacker.
+    </p>
+    <p>
+      I've been in the game for about fifteen years now and have coded full stack to more recently as a full-on
+      front-end engineer. What can I say? I love to code!
+    </p>
+  </Layout>
 );
