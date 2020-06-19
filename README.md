3a22cba9f683e1392a217fd6dcac00768fbd4273
Michaux Kelley <michauxkelley@gmail.com>
2020-06-18 23:40:50 -0400

    Add contact form with recaptcha

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ This site/app is a work in progress.
 
 # Getting Started
 
+1. **.env**
+
+You'll need to create a `.env.development` file at a minimum to integrate the
+client with the server. In it you should type
+`GATSBY_API_URL=http://localhost:8080` or whereever you're running the server.
+
+You'll also need to assign `RECAPTCHA_SITE_KEY` for the reCAPTCHA functionality.
+Additionally, create the server `.env.development` in the server directory, and set
+`RECAPTCHA_SECRET_KEY` You can obtain values for these keys respectively by going
+to https://www.google.com/recaptcha/admin.
+
 1. `cd` into `client` and run `npm install`
 1. Then, run `gatsby develop`
 1. From the root directory `cd` into `server` and run `npm install`
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,10 +1,130 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
-export default () => (
-  <div>
-    <h1>I'd love to talk! Email me at the address below</h1>
-    <p>
-      <a href="mailto:me@example.com">me@example.com</a>
-    </p>
-  </div>
-);
+import Layout from '@components/layout';
+import api from '../api';
+import TextInput from '@components/common/forms/TextInput';
+import TextArea from '@components/common/forms/TextArea';
+
+const formikEnhancer = withFormik({
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().min(2, "C'mon, your first name is longer than that").required('First name is required'),
+    lastName: Yup.string().min(2, "C'mon, your last name is longer than that").required('Last name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    message: Yup.string().required('Message is required'),
+    recaptcha: Yup.string().required(),
+  }),
+  handleSubmit: (payload, { setSubmitting }) => {
+    console.log('payload', payload);
+    api
+      .put(`/contact`, payload, {
+        withCredentials: true,
+      })
+      .then(res => {
+        toast.success('Your message has been sent', {
+          position: toast.POSITION.TOP_CENTER,
+          hideProgressBar: true,
+        });
+      })
+      .catch(err => {
+        toast.error('Something went wrong', {
+          position: toast.POSITION.TOP_CENTER,
+          hideProgressBar: true,
+        });
+      });
+    setSubmitting(false);
+  },
+  mapPropsToValues: () => ({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+    recaptcha: '',
+  }),
+  displayName: 'ContactForm',
+});
+
+const ContactForm = props => {
+  const tokenEl = useRef(null);
+
+  document.title = 'Contact Form';
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue } = props;
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    script.defer = true;
+    window.onSubmit = token => {
+      setFieldValue('recaptcha', token);
+      // TODO: verify against server
+      console.log('verify against server');
+    };
+    window.onExpired = () => setFieldValue('recaptcha', '');
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <Layout>
+      <section>
+        <h1>Contact</h1>
+        <form onSubmit={handleSubmit}>
+          <TextInput
+            id="firstName"
+            type="text"
+            label="First Name"
+            error={touched.firstName && errors.firstName}
+            value={values.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextInput
+            id="lastName"
+            type="text"
+            label="Last Name"
+            error={touched.lastName && errors.lastName}
+            value={values.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextInput
+            id="email"
+            type="email"
+            label="Email"
+            autoComplete="username email"
+            error={touched.email && errors.email}
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextArea
+            id="message"
+            type="text"
+            label="Message"
+            error={touched.message && errors.message}
+            value={values.message}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            rows={10}
+          />
+          <input id="recaptcha" name="recaptcha" type="hidden" ref={tokenEl} value="" />
+          <div className="form-group">
+            <div
+              className="g-recaptcha"
+              data-sitekey={process.env.RECAPTCHA_SITE_KEY}
+              data-callback="onSubmit"
+              data-expired-callback="onExpired"
+            ></div>
+          </div>
+          <button type="submit" disabled={isSubmitting} className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </section>
+    </Layout>
+  );
+};
+
+export default formikEnhancer(ContactForm);
