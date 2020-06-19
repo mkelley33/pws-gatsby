8ed836ce7f12bd55d0fe1fbd3c7f00d9fa340d80
Michaux Kelley <michauxkelley@gmail.com>
2020-06-18 23:36:57 -0400

    Remove unnecessary comment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const ax = axios.create({
-  // TODO: make configurable based on whether this is dev or production
   baseURL: `${process.env.GATSBY_API_URL}/api/v1`,
   withCredentials: true,
 });
