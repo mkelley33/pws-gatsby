c1ce9d5cfb97297cbcab655b5bab47fdf980b7e2
Michaux Kelley <michauxkelley@gmail.com>
2020-06-24 20:41:29 -0400

    Add production config for server

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,17 +1,25 @@
 export default {
-  host: 'localhost',
+  host: process.env.HOST,
   env: 'production',
+  protocol: 'https',
+  mail: {
+    user: 'michauxkelley@gmail.com',
+    pass: process.env.EMAIL_PASS,
+    sender: 'Michaux Kelley <michauxkelley@gmail.com>',
+  },
+  MONGOOSE_DEBUG: true,
   jwtSecret: process.env.JWT_SECRET,
   db: {
-    uri: 'mongodb://localhost/api-production',
+    uri: `mongodb+srv://pws-user:${process.env.PWS_PASS}@cluster0-plswm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     options: {
-      useMongoClient: true,
+      useNewUrlParser: true,
       keepAlive: true,
       socketTimeoutMS: 0,
-      reconnectTries: 30,
-      user: process.env.LHS_USER,
-      pass: process.env.LHS_PASS,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      user: process.env.PWS_USER,
+      pass: process.env.PWS_PASS,
     },
   },
-  port: 4040,
+  port: 443,
 };
