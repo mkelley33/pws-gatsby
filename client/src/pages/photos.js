f1373ef503285dfd5bcd5ce398e4c8b39da99a11
Michaux Kelley <michauxkelley@gmail.com>
2020-07-20 16:39:15 -0400

    Refactor photos to be a functional component

--- client/src/pages/photos.js
+++ client/src/pages/photos.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import classnames from 'classnames';
 import Pagination from 'react-js-pagination';
@@ -18,53 +18,53 @@ const AddAlbumLink = props => {
   );
 };
 
-class Photos extends Component {
-  state = {
-    photos: [],
-    albums: [],
-    pageCount: 1,
-    pageRange: 1,
-    perPage: 9,
-    offset: 0,
-  };
+const Photos = () => {
+  const [photos, setPhotos] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [pageRange, setPageRange] = useState(1);
+  const [perPage, setPerPage] = useState(9);
+  const [offset, setOffset] = useState(0);
+  const [isAlbumClick, setIsAlbumClick] = useState(false);
+  const [albumId, setAlbumId] = useState(null);
+  const [albumName, setAlbumName] = useState(null);
+  const [showViewAllPhotos, setShowViewAllPhotos] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const [activePage, setActivePage] = useState(0);
 
-  getPhotos = id => {
-    if (this.state.isAlbumClick) {
-      return api.get(`/photo-albums/${id}`, { params: { limit: this.state.perPage, skip: this.state.offset } });
+  const getPhotos = id => {
+    if (isAlbumClick) {
+      return api.get(`/photo-albums/${id}`, { params: { limit: perPage, skip: offset } });
     } else {
-      return api.get('/photos', { params: { limit: this.state.perPage, skip: this.state.offset } });
+      return api.get('/photos', { params: { limit: perPage, skip: offset } });
     }
   };
 
-  handlePrivateAlbumsClick = () => {
+  const handlePrivateAlbumsClick = () => {
     api.get('/photo-albums/', { params: { isPublic: false } }).then(response => {
-      this.setState({ albums: response.data });
+      setAlbums(response.data);
     });
   };
 
-  handleSharedAlbumsClick = () => {
+  const handleSharedAlbumsClick = () => {
     api.get('/photo-albums/', { params: { isPublic: true } }).then(response => {
-      this.setState({ albums: response.data });
+      setAlbums(response.data);
     });
   };
 
-  handleAllAlbumsClick = () => {
+  const handleAllAlbumsClick = () => {
     api.get('/photo-albums/').then(response => {
-      this.setState({ albums: response.data });
+      setAlbums(response.data);
     });
   };
 
-  componentDidMount() {
-    Promise.all([this.getPhotos(), api.get('/photo-albums')])
+  useEffect(() => {
+    Promise.all([getPhotos(), api.get('/photo-albums')])
       .then(res => {
-        const pageCount = Math.ceil(res[0].data.totalCount / this.state.perPage);
-        this.setState({
-          photos: res[0].data.photos,
-          totalCount: res[0].data.totalCount,
-          pageCount,
-          pageRange: pageCount <= 9 ? pageCount : 9,
-          albums: res[1].data,
-        });
+        const pageCount = Math.ceil(res[0].data.totalCount / perPage);
+        setPhotos(res[0].data.photos);
+        setTotalCount(res[0].data.totalCount);
+        setPageRange(pageCount <= 9 ? pageCount : 9);
+        setAlbums(res[1].data);
       })
       .catch(err => {
         toast.error('Something went wrong', {
@@ -72,16 +72,16 @@ class Photos extends Component {
           hideProgressBar: true,
         });
       });
-  }
+  });
 
-  deletePhoto = id => {
+  const deletePhoto = id => {
     api.delete(`/photos/${id}`).then(res => {
-      const photoIndex = this.state.photos.findIndex(photo => {
+      const photoIndex = photos.findIndex(photo => {
         return photo._id === id;
       });
-      const photos = [...this.state.photos];
+      const photos = [...photos];
       photos.splice(photoIndex, 1);
-      this.setState({ photos });
+      setPhotos(photos);
       toast.success('You successfully deleted the photo', {
         position: toast.POSITION.TOP_CENTER,
         hideProgressBar: true,
@@ -89,178 +89,175 @@ class Photos extends Component {
     });
   };
 
-  deleteAlbum = id => {
+  const deleteAlbum = id => {
     api.delete(`/photo-albums/${id}`).then(res => {
-      const albumIndex = this.state.albums.findIndex(album => {
+      const albumIndex = albums.findIndex(album => {
         return album._id === id;
       });
-      const albums = [...this.state.albums];
+      const albums = [...albums];
       albums.splice(albumIndex, 1);
-      this.setState({ albums });
-      this.handleViewAllPhotosClick();
+      setAlbums(albums);
+      handleViewAllPhotosClick();
     });
   };
 
-  handleAlbumClick = event => {
+  const handleAlbumClick = event => {
     const { id } = event.target.dataset;
-    this.setState({ offset: 0, activePage: 1, isAlbumClick: true, albumId: id, showViewAllPhotos: true }, () => {
-      this.getPhotos(id).then(res => {
-        const pageCount = Math.ceil(res.data.totalCount / this.state.perPage);
-        this.setState({
-          photos: res.data.photos.map(photo => photo.photos),
-          totalCount: res.data.totalCount,
-          pageRange: pageCount <= 9 ? pageCount : 9,
-          albumName: res.data.photos[0].name,
-        });
+    setOffset(0);
+    setActivePage(1);
+    setIsAlbumClick(true);
+    setAlbumId(id);
+    setShowViewAllPhotos(true, () => {
+      getPhotos(id).then(res => {
+        const pageCount = Math.ceil(res.data.totalCount / perPage);
+        setPhotos(res.data.photos.map(photo => photo.photos));
+        setTotalCount(res.data.totalCount);
+        setPageRange(pageCount <= 9 ? pageCount : 9);
+        setAlbumName(res.data.photos[0].name);
       });
     });
   };
 
-  handlePageChange = pageNumber => {
-    const offset = (pageNumber - 1) * this.state.perPage;
-    this.setState({ offset, activePage: pageNumber }, () => {
-      this.getPhotos(this.state.albumId).then(res => {
+  const handlePageChange = pageNumber => {
+    const offset = (pageNumber - 1) * perPage;
+    setOffset(offset);
+    setActivePage(pageNumber, () => {
+      getPhotos(albumId).then(res => {
         if (res.data && res.data.photos && res.data.photos[0] && res.data.photos[0].photos) {
-          this.setState({
-            photos: res.data.photos.map(photo => photo.photos),
-            totalCount: res.data.totalCount,
-          });
+          setPhotos(res.data.photos.map(photo => photo.photos));
+          setTotalCount(res.data.totalCount);
         } else {
-          this.setState({
-            photos: res.data.photos,
-            totalCount: res.data.totalCount,
-          });
+          setPhotos(res.data.photos);
+          setTotalCount(res.data.totalCount);
         }
       });
     });
   };
 
-  handleViewAllPhotosClick = () => {
-    this.setState({ albumId: undefined, isAlbumClick: false, albumName: undefined, showViewAllPhotos: false }, () =>
-      this.getPhotos().then(res => {
-        const pageCount = Math.ceil(res.data.totalCount / this.state.perPage);
-        this.setState({
-          photos: res.data.photos,
-          totalCount: res.data.totalCount,
-          pageCount,
-          pageRange: pageCount <= 9 ? pageCount : 9,
-        });
+  const handleViewAllPhotosClick = () => {
+    setAlbumId(undefined);
+    setIsAlbumClick(false);
+    setAlbumName(undefined);
+    setShowViewAllPhotos(false, () =>
+      getPhotos().then(res => {
+        const pageCount = Math.ceil(res.data.totalCount / perPage);
+        setPhotos(res.data.photos);
+        setTotalCount(res.data.totalCount);
+        setPageRange(pageCount <= 9 ? pageCount : 9);
       })
     );
   };
 
-  render() {
-    // TODO: show loader for photo gallery
-    return (
-      <Layout>
-        <div className={styles.container}>
-          <div className={styles.albumsContainer}>
-            <h2 className={styles.albumsHeader}>Albums</h2>
-            <nav>
-              <ul className={styles.albumsMenu}>
-                <li className={styles.albumsMenuItem}>
-                  <button onClick={this.handleAllAlbumsClick} type="button" className="btn btn-link">
-                    All
-                  </button>{' '}
-                  |
-                </li>
-                <li className={styles.albumsMenuItem}>
-                  <button onClick={this.handleSharedAlbumsClick} type="button" className="btn btn-link">
-                    Shared
-                  </button>
-                  |
-                </li>
-                <li className={styles.albumsMenuItem}>
-                  <button onClick={this.handlePrivateAlbumsClick} type="button" className="btn btn-link">
-                    Private
-                  </button>
-                </li>
-              </ul>
-            </nav>
-            {this.state.albums.length ? (
-              <AddAlbumLink />
-            ) : (
-              <React.Fragment>
-                <p>You don't have any albums. Add one below.</p>
-                <AddAlbumLink />
-              </React.Fragment>
-            )}
-            {this.state.albums &&
-              this.state.albums.map(album => {
-                if (!album.photos.length) return;
-
-                return (
-                  <React.Fragment key={album._id}>
-                    <img
-                      onClick={this.handleAlbumClick}
-                      data-id={album._id}
-                      key={album._id}
-                      className={styles.albumThumb}
-                      src={`${process.env.GATSBY_API_URL}/images/${album.photos[0].userId}/thumbs/${album.photos[0].filename}`}
-                      alt=""
-                    />
-                    <div className={styles.albumName}>{album.name}</div>
-                    <ModalConfirm
-                      modalButtonClassName={classnames('btn', 'btn-link', styles.deleteButton)}
-                      confirmArgument={album._id}
-                      onConfirm={this.deleteAlbum}
-                      modalContent={<p>Are you sure you want to delete this album?</p>}
-                      modalProps={{ triggerText: 'Delete' }}
-                    />
-                  </React.Fragment>
-                );
-              })}
-          </div>
-          <div>
-            <h2>Photo Gallery</h2>
-            {this.state.showViewAllPhotos && (
-              <p>
-                <button className="btn btn-link" onClick={this.handleViewAllPhotosClick}>
-                  View All My Photos
+  // TODO: show loader for photo gallery
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.albumsContainer}>
+          <h2 className={styles.albumsHeader}>Albums</h2>
+          <nav>
+            <ul className={styles.albumsMenu}>
+              <li className={styles.albumsMenuItem}>
+                <button onClick={handleAllAlbumsClick} type="button" className="btn btn-link">
+                  All
+                </button>{' '}
+                |
+              </li>
+              <li className={styles.albumsMenuItem}>
+                <button onClick={handleSharedAlbumsClick} type="button" className="btn btn-link">
+                  Shared
                 </button>
-              </p>
-            )}
-            <p>
-              <Link to="/photo-upload">Upload Photos</Link>
-            </p>
-            {this.state.albumName && <h3>Album: {this.state.albumName}</h3>}
-            <div className={styles.thumbsGrid}>
-              {this.state.photos.map(photo => {
-                return (
-                  <div key={photo._id} className={styles.thumbContainer}>
-                    <img
-                      className={styles.thumb}
-                      src={`${process.env.GATSBY_API_URL}/images/${photo.userId}/thumbs/${photo.filename}`}
-                      alt=""
-                    />
-                    <ModalConfirm
-                      modalButtonClassName={classnames('btn', 'btn-link', styles.deleteButton)}
-                      confirmArgument={photo._id}
-                      onConfirm={this.deletePhoto}
-                      modalContent={<p>Are you sure you want to delete this photo?</p>}
-                      modalProps={{ triggerText: 'Delete' }}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-            {this.state.pageRange > 1 && (
-              <Pagination
-                activePage={this.state.activePage}
-                activeClass={styles.activePage}
-                itemClass={styles.paginationItem}
-                disabledClass={styles.disabled}
-                itemsCountPerPage={this.state.perPage}
-                totalItemsCount={this.state.totalCount}
-                pageRangeDisplayed={this.state.pageRange}
-                onChange={this.handlePageChange}
-              />
-            )}
-          </div>
+                |
+              </li>
+              <li className={styles.albumsMenuItem}>
+                <button onClick={handlePrivateAlbumsClick} type="button" className="btn btn-link">
+                  Private
+                </button>
+              </li>
+            </ul>
+          </nav>
+          {albums.length ? (
+            <AddAlbumLink />
+          ) : (
+            <React.Fragment>
+              <p>You don't have any albums. Add one below.</p>
+              <AddAlbumLink />
+            </React.Fragment>
+          )}
+          {albums &&
+            albums.map(album => {
+              if (!album.photos.length) return;
+
+              return (
+                <React.Fragment key={album._id}>
+                  <img
+                    onClick={handleAlbumClick}
+                    data-id={album._id}
+                    key={album._id}
+                    className={styles.albumThumb}
+                    src={`${process.env.GATSBY_API_URL}/images/${album.photos[0].userId}/thumbs/${album.photos[0].filename}`}
+                    alt=""
+                  />
+                  <div className={styles.albumName}>{album.name}</div>
+                  <ModalConfirm
+                    modalButtonClassName={classnames('btn', 'btn-link', styles.deleteButton)}
+                    confirmArgument={album._id}
+                    onConfirm={deleteAlbum}
+                    modalContent={<p>Are you sure you want to delete this album?</p>}
+                    modalProps={{ triggerText: 'Delete' }}
+                  />
+                </React.Fragment>
+              );
+            })}
         </div>
-      </Layout>
-    );
-  }
-}
+        <div>
+          <h2>Photo Gallery</h2>
+          {showViewAllPhotos && (
+            <p>
+              <button className="btn btn-link" onClick={handleViewAllPhotosClick}>
+                View All My Photos
+              </button>
+            </p>
+          )}
+          <p>
+            <Link to="/photo-upload">Upload Photos</Link>
+          </p>
+          {albumName && <h3>Album: {albumName}</h3>}
+          <div className={styles.thumbsGrid}>
+            {photos.map(photo => {
+              return (
+                <div key={photo._id} className={styles.thumbContainer}>
+                  <img
+                    className={styles.thumb}
+                    src={`${process.env.GATSBY_API_URL}/images/${photo.userId}/thumbs/${photo.filename}`}
+                    alt=""
+                  />
+                  <ModalConfirm
+                    modalButtonClassName={classnames('btn', 'btn-link', styles.deleteButton)}
+                    confirmArgument={photo._id}
+                    onConfirm={deletePhoto}
+                    modalContent={<p>Are you sure you want to delete this photo?</p>}
+                    modalProps={{ triggerText: 'Delete' }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          {pageRange > 1 && (
+            <Pagination
+              activePage={activePage}
+              activeClass={styles.activePage}
+              itemClass={styles.paginationItem}
+              disabledClass={styles.disabled}
+              itemsCountPerPage={perPage}
+              totalItemsCount={totalCount}
+              pageRangeDisplayed={pageRange}
+              onChange={handlePageChange}
+            />
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default withAuthentication(Photos);
